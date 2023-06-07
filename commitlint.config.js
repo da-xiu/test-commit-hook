418af08b164183ce762dbeb84396c1781de4b7d9
guoxiu <guox03@mingyuanyun.com>
2023-06-07 20:44:08 +0800

    fdfdf

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum':[ 2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert']],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+    'header-max-length': [0, 'always', 72]
+  }
 }
